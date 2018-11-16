a2abead879a10e37fcb795db64fbe1f3539824ea
luosheng <chubalh@live.com>
2018-11-16 10:41:33 +0800

    feat: update asciidoc ,add user query

--- fo-up/src/main/java/com/fo/up/entity/UpOrganization.java
+++ fo-up/src/main/java/com/fo/up/entity/UpOrganization.java
@@ -1,10 +1,12 @@
 package com.fo.up.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class UpOrganization {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- fo-up/src/main/java/com/fo/up/repository/UpOrganizationRepository.java
+++ fo-up/src/main/java/com/fo/up/repository/UpOrganizationRepository.java
@@ -1,7 +1,5 @@
 package com.fo.up.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,18 +7,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fo.up.entity.UpOrganization;
-import com.fo.up.entity.UpPermission;
 
 public interface UpOrganizationRepository extends JpaRepository<UpOrganization, Long>{
     @Transactional
     @Modifying(clearAutomatically = true)
-    @Query("update UpOrganization o set  "
+    @Query(value = "update up_organization o set  "
             + " o.name = CASE WHEN :#{#upOrganization.name} IS NULL THEN o.name ELSE :#{#upOrganization.name} END ,"
             + " o.pid = CASE WHEN :#{#upOrganization.title} IS NULL THEN o.title ELSE :#{#upOrganization.title} END ,"
             + " o.description = CASE WHEN :#{#upOrganization.description} IS NULL THEN o.description ELSE :#{#upOrganization.description} END,"
-            + " o.ctime = CASE WHEN :#{#upOrganization.ctime} IS NULL THEN o.ctime ELSE :#{#upOrganization.ctime} END,"
-            + " where r.organizationId = :#{#upOrganization.organizationId}")
-    public void updateByUpOrganization(UpOrganization upOrganization);
+            + " o.ctime = CASE WHEN :#{#upOrganization.ctime} IS NULL THEN o.ctime ELSE :#{#upOrganization.ctime} END"
+            + " where o.organization_id = :#{#upOrganization.organizationId}", nativeQuery = true)
+    public void updateByUpOrganization(@Param("upOrganization")UpOrganization upOrganization);
     
    
 }
