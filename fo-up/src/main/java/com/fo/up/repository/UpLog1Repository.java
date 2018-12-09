4abb9bbf820e8bd39fb825abcd7fd7c7bdad8a36
luosheng <chubalh@live.com>
2018-12-09 21:54:00 +0800

    feat: update log page method

--- fo-up/src/main/java/com/fo/up/repository/UpLog1Repository.java
+++ fo-up/src/main/java/com/fo/up/repository/UpLog1Repository.java
@@ -1,14 +1,14 @@
 package com.fo.up.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fo.up.entity.UpLog1;
-import com.fo.up.entity.UpUser;
 
 public interface UpLog1Repository extends JpaRepository<UpLog1, Long> {
+    Page<UpLog1> findAll(Specification<UpLog1> spec, Pageable pageable);
+
    
 }
