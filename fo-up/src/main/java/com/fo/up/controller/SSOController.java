7bbfcf3f61145cff4d8dff7c0d252680e688fcfb
luosheng <chubalh@live.com>
2019-03-27 23:17:35 +0800

    feat: add param validate

--- fo-up/src/main/java/com/fo/up/controller/SSOController.java
+++ fo-up/src/main/java/com/fo/up/controller/SSOController.java
@@ -1,16 +1,15 @@
 package com.fo.up.controller;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fo.up.entity.UpUser;
@@ -20,37 +19,38 @@ import com.fo.up.entity.UpUser;
 public class SSOController {
 
     @PostMapping("/login")
-    public String login(@RequestBody UpUser upUser) {
+    public String login(@Valid @RequestBody UpUser upUser) {
         Subject currentUser = SecurityUtils.getSubject();
-        if ( !currentUser.isAuthenticated() ) {
-            //collect user principals and credentials in a gui specific manner
-            //such as username/password html form, X509 certificate, OpenID, etc.
-            //We'll use the username/password example here since it is the most common.
-            UsernamePasswordToken token = new UsernamePasswordToken(upUser.getUsername(), upUser.getPassword());
-
-            //this is all you have to do to support 'remember me' (no config - built in!):
-            token.setRememberMe(true);
-            currentUser.login(token);
-            return  "login sucess";
+        // login already
+        if (currentUser.isAuthenticated()) {
+            return "already login";
         }
-        return "already login";
+        //collect user principals and credentials in a gui specific manner
+        //such as username/password html form, X509 certificate, OpenID, etc.
+        //We'll use the username/password example here since it is the most common.
+        UsernamePasswordToken token = new UsernamePasswordToken(upUser.getUsername(), upUser.getPassword());
+        //this is all you have to do to support 'remember me' (no config - built in!):
+        token.setRememberMe(true);
+        currentUser.login(token);
+        return  "login sucess";
     }
 
     @GetMapping("/login")
-    public String login(@Validated @NotEmpty String username,@Valid  @NotEmpty String password) {
+    public String login(@RequestParam(name = "username", required = true) String username,@RequestParam(name = "password", required = true) String password) {
         Subject currentUser = SecurityUtils.getSubject();
-        if ( !currentUser.isAuthenticated() ) {
-            //collect user principals and credentials in a gui specific manner
-            //such as username/password html form, X509 certificate, OpenID, etc.
-            //We'll use the username/password example here since it is the most common.
-            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-            
-            //this is all you have to do to support 'remember me' (no config - built in!):
-            token.setRememberMe(true);
-            currentUser.login(token);
-            return  "login sucess";
+        if ( currentUser.isAuthenticated() ) {
+            return "already login";
         }
-        return "already login";
+        //collect user principals and credentials in a gui specific manner
+        //such as username/password html form, X509 certificate, OpenID, etc.
+        //We'll use the username/password example here since it is the most common.
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        
+        //this is all you have to do to support 'remember me' (no config - built in!):
+        token.setRememberMe(true);
+        currentUser.login(token);
+        return  "login sucess";
+        
     }
 
     @GetMapping("/role")
--- fo-up/src/main/java/com/fo/up/entity/UpUser.java
+++ fo-up/src/main/java/com/fo/up/entity/UpUser.java
@@ -12,6 +12,7 @@ import javax.persistence.Id;
 public class UpUser implements Serializable{
 
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;// 编号
