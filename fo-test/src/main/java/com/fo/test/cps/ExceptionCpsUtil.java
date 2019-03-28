cd107f394bdc3fd793d76f2547d02b8bb654d3a4
luosheng <chubalh@live.com>
2019-03-28 20:08:16 +0800

    feat: update test

--- fo-test/src/main/java/com/fo/test/cps/ExceptionCpsUtil.java
+++ fo-test/src/main/java/com/fo/test/cps/ExceptionCpsUtil.java
@@ -24,11 +24,36 @@ public class ExceptionCpsUtil {
         }
     }
 
+    public static void trowException1(Boolean a, Exception e, Consumer<Exception> f) {
+        if (!a) {
+            log.info(e.getMessage(), e);
+            f.accept(e);
+        }
+    }
+
     public static void trowException(RuntimeException e) {
         throw e;
     }
 
+    public static void ss(Exception e) throws Exception {
+        throw e;
+    }
+
+    /**
+     * throw Exceptoin instance in lambda use Generic type
+     * 
+     * @param e
+     * @throws E
+     */
+    @SuppressWarnings("unchecked")
+    static <E extends Exception> void throwException(Exception e) throws E {
+        throw (E)e;
+    }
+
     public static void main(String[] args) {
-        trowException(false, new RuntimeException("xx"), ExceptionCpsUtil::trowException);
+//        trowException(false, new RuntimeException("xx"), ExceptionCpsUtil::trowException);
+        trowException1(false, new Exception(), e -> {
+            throwException(e);
+        });
     }
 }
